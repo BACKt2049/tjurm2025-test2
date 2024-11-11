e8f8052b237bfbef2103aab4c31b514a58f75b59
BACKt2049 <2199480387@qq.com>
2024-11-11 16:59:03 +0800

    Update impl.cc

--- src/roi_color/impl.cc
+++ src/roi_color/impl.cc
@@ -1,35 +1,51 @@
 #include "impls.h"
+#include <opencv2/opencv.hpp>
 #include <unordered_map>
 
-
 std::unordered_map<int, cv::Rect> roi_color(const cv::Mat& input) {
-    /**
-     * INPUT: 一张彩色图片, 路径: opencv/assets/roi_color/input.png
-     * OUTPUT: 一个 unordered_map, key 为颜色(Blue: 0, Green: 1, Red: 2), value 为对应颜色的矩形区域(cv::Rect)
-     * TODO:
-     *  1. 找到图片中三个颜色的轮廓
-     *  2. 计算出三处矩形的颜色
-     *  3. 返回一个 unordered_map, key 为颜色, value 为cv::Rect (表示矩形的位置)
-     * HINT:
-     *  1. 预处理
-     *    1. 在学习 findContours 时，我们提供的输入图片已经是一个二值图像了，
-     *       但是在这里，input 是一个彩色图形。
-     *       而 findContours 的 input 最好是一个二值图像。
-     *    2. findContours 处理时，一般是在一个黑色背景上，找白色的轮廓
-     *       (因此在二值化时，你可能需要考虑一下 cv::THRESH_BINARY_INV 选项
-     *       同时，建议了解一下 cv::THRESH_OTSU 选项以及他们如何配合使用)
-     *    3. 因此，预处理流程如下:
-     *      1. 将 input 转换成灰度图像
-     *      2. 对灰度图像进行二值化，得到黑底的二值化图
-     *   2. 找到三个矩形 (findContours)
-     *   3. 对找到的三个矩形，分别进行如下计算:
-     *      1. 使用 cv::boundingRect 将轮廓转换成矩形 cv::Rect
-     *      2. 使用该 cv::Rect 得到 input 中的 ROI区域 (写法: roi = input(rect))
-     *      3. 使用统计的方法，得到该 ROI 区域的颜色
-     *      4. 将颜色 和 矩形位置 存入 map 中
-     */
     std::unordered_map<int, cv::Rect> res;
-    // IMPLEMENT YOUR CODE HERE
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+
+    // 1. 将彩色图像转换为灰度图像
+    cv::Mat gray_image;
+    cv::cvtColor(input, gray_image, cv::COLOR_BGR2GRAY);
+
+    // 2. 对灰度图像进行二值化
+    cv::Mat binary_image;
+    cv::threshold(gray_image, binary_image, 0, 255, cv::THRESH_BINARY_INV + cv::THRESH_OTSU);
+
+    // 3. 使用cv::findContours函数检测轮廓
+    cv::findContours(binary_image, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    // 遍历所有轮廓
+    for (size_t i = 0; i < contours.size(); i++) {
+        // 4. 使用cv::boundingRect计算外接矩形
+        cv::Rect rect = cv::boundingRect(contours[i]);
+
+        // 5. 使用该cv::Rect得到input中的ROI区域
+        cv::Mat roi = input(rect);
+
+        // 6. 使用统计的方法，得到该ROI区域的颜色
+        int blue = 0, green = 0, red = 0;
+        for (int y = 0; y < roi.rows; y++) {
+            for (int x = 0; x < roi.cols; x++) {
+                cv::Vec3b color = roi.at<cv::Vec3b>(y, x);
+                red += color[2];
+                green += color[1];
+                blue += color[0];
+            }
+        }
+        int totalPixels = roi.rows * roi.cols;
+        int maxColor = std::max({blue, green, red});
+        if (maxColor == blue) {
+            res[0] = rect; // Blue
+        } else if (maxColor == green) {
+            res[1] = rect; // Green
+        } else if (maxColor == red) {
+            res[2] = rect; // Red
+        }
+    }
 
     return res;
 }
