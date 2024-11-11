d4e8f426e138b3b294ce5a2c76de431de13cb3d3
BACKt2049 <2199480387@qq.com>
2024-11-11 21:11:05 +0800

    Update impl.cc

--- src/compute_iou/impl.cc
+++ src/compute_iou/impl.cc
@@ -1,11 +1,10 @@
 #include "impls.h"
 #include <algorithm>
-
 float compute_iou(const cv::Rect& a, const cv::Rect& b) 
 {
      cv::Rect chonghe = a & b;
     float chonghearea = chonghe.width * chonghe.height;
     float bingjiarea = a.area() + b.area() - chonghearea;
     float iou = chonghearea / bingjiarea;
-    return 0.f;
+  return iou;
 }
