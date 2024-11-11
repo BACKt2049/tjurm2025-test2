1a1034be3da71fc8de70d132ac84fceacf32fb40
BACKt2049 <2199480387@qq.com>
2024-11-11 16:55:05 +0800

    Update impl.cc

--- src/find_contours/impl.cc
+++ src/find_contours/impl.cc
@@ -1,23 +1,19 @@
 #include "impls.h"
 
-
 std::vector<std::vector<cv::Point>> find_contours(const cv::Mat& input) {
-    /**
-     * 要求：
-     * 使用cv::findContours函数，从输入图像（3个通道）中找出所有的最内层轮廓。
-     * 将它们保存起来作为函数返回值就行。contours的中文意思就是轮廓。
-     * 
-     * 提示：
-     * 1. 使用cv::imshow来查看输入图像。
-     * 2. 使用cv::drawContours来在一张图上绘制轮廓。
-     * 3. 直接使用原图一般而言不利于轮廓的寻找，可以做一些简单的处理。
-     * 4. findContours函数可以返回轮廓的层次结构，理解层次结构的保存方式并使用它（重点）。
-     * 
-     * 通过条件：
-     * 运行测试点，你找到的轮廓与答案的轮廓一样就行。
-     */
-    
     std::vector<std::vector<cv::Point>> res;
-    // IMPLEMENT YOUR CODE HERE
+    std::vector<cv::Vec4i> hierarchy;
+
+    // 1. 将彩色图像转换为灰度图像
+    cv::Mat gray_image;
+    cv::cvtColor(input, gray_image, cv::COLOR_BGR2GRAY);
+
+    // 2. 对灰度图像进行二值化
+    cv::Mat binary_image;
+    cv::threshold(gray_image, binary_image, 100, 255, cv::THRESH_BINARY);
+
+    // 3. 使用cv::findContours函数检测轮廓
+    cv::findContours(binary_image, res, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
     return res;
 }
