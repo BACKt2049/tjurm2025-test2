7b2ecd6662ef814c47e9fabdf67d92a0c545f405
BACKt2049 <2199480387@qq.com>
2024-11-11 21:09:49 +0800

    Update impl.cc

--- src/area_ratio/impl.cc
+++ src/area_ratio/impl.cc
@@ -1,9 +1,10 @@
-#include "impls.h"
-float compute_area_ratio(const std::vector<cv::Point>& contour) 
-{
-     double contourArea = cv::contourArea(contour);
-    cv::Rect boundingRect = cv::boundingRect(contour);
-    double rectArea = boundingRect.width * boundingRect.height;
-    float areaRatio = static_cast<float>(contourArea) / rectArea;
-    return 0.f;
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+float compute_area_ratio(const std::vector<cv::Point>& contour) {
+    double carea = cv::contourArea(contour);
+    cv::Rect boundingrect = cv::boundingRect(contour);
+    double rectarea = boundingrect.width * boundingrect.height;
+    float arear = static_cast<float>(carea) / rectarea;
+    return arear;
 }
