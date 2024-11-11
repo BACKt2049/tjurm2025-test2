6e273a51035ad0cea398ae204d87c0c057422385
BACKt2049 <2199480387@qq.com>
2024-11-11 19:13:27 +0800

    Update impl.cc

--- src/find_contours/impl.cc
+++ src/find_contours/impl.cc
@@ -3,17 +3,10 @@
 std::vector<std::vector<cv::Point>> find_contours(const cv::Mat& input) {
     std::vector<std::vector<cv::Point>> res;
     std::vector<cv::Vec4i> hierarchy;
-
-    // 1. 将彩色图像转换为灰度图像
     cv::Mat gray_image;
     cv::cvtColor(input, gray_image, cv::COLOR_BGR2GRAY);
-
-    // 2. 对灰度图像进行二值化
     cv::Mat binary_image;
     cv::threshold(gray_image, binary_image, 100, 255, cv::THRESH_BINARY);
-
-    // 3. 使用cv::findContours函数检测轮廓
     cv::findContours(binary_image, res, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
     return res;
 }
