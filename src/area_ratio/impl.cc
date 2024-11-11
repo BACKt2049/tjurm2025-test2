18e4b4dcbc48602ac14b15e8b2cc9041537a23cd
BACKt2049 <2199480387@qq.com>
2024-11-11 18:48:22 +0800

    Update impl.cc

--- src/area_ratio/impl.cc
+++ src/area_ratio/impl.cc
@@ -2,26 +2,9 @@
 
 
 float compute_area_ratio(const std::vector<cv::Point>& contour) {
-    /**
-     * 要求：
-     *      计算输入的轮廓的面积与它的最小外接矩形面积的比例。自行查找
-     * 获得轮廓面积的函数。这个量可以用来判断一个轮廓到底有多靠近一个矩形。
-     * 
-     * 提示：
-     * 无。
-     * 
-     * 通过条件:
-     * 运行测试点，通过即可。
-     */
      double contourArea = cv::contourArea(contour);
-    
-    // 获取轮廓的最小外接矩形
     cv::Rect boundingRect = cv::boundingRect(contour);
-    
-    // 计算最小外接矩形的面积
     double rectArea = boundingRect.width * boundingRect.height;
-    
-    // 计算面积比例
     float areaRatio = static_cast<float>(contourArea) / rectArea;
     return 0.f;
 }
