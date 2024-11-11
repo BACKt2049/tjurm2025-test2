3db9fd7b130a5cb86aaf84ad017081647a570ea7
BACKt2049 <2199480387@qq.com>
2024-11-11 20:49:59 +0800

    Update impl.cc

--- src/threshold/impl.cc
+++ src/threshold/impl.cc
@@ -1,26 +1,22 @@
 #include <vector>
 #include <algorithm>
-
-std::vector<cv::Mat> threshold(const cv::Mat& src, int threshold_value) {
-    cv::Mat gray(src.size(), CV_8UC1); // 创建灰度图像
-    cv::Mat dst(src.size(), CV_8UC1);  // 创建二值化图像
-
-    // 将 src 转换成灰度图像
-    for (int y = 0; y < src.rows; ++y) {
-        for (int x = 0; x < src.cols; ++x) {
-            cv::Vec3b color = src.at<cv::Vec3b>(y, x);
+std::vector<cv::Mat> threshold(const cv::Mat& src, int threshold_value) 
+{
+    cv::Mat gray(src.size(), CV_8UC1); 
+    cv::Mat dst(src.size(), CV_8UC1);  
+    for (int y = 0; y < src.rows; ++y)
+        {for (int x = 0; x < src.cols; ++x)
+            {cv::Vec3b color = src.at<cv::Vec3b>(y, x);
             uchar grayValue = static_cast<uchar>(0.299 * color[2] + 0.587 * color[1] + 0.114 * color[0]);
-            gray.at<uchar>(y, x) = grayValue;
+            gray.at<uchar>(y, x) = grayValue; }
         }
-    }
-
-    // 阈值化转换为二值化图像
-    for (int y = 0; y < src.rows; ++y) {
-        for (int x = 0; x < src.cols; ++x) {
+    for (int y = 0; y < src.rows; ++y)
+        {
+        for (int x = 0; x < src.cols; ++x) 
+        {
             uchar grayValue = gray.at<uchar>(y, x);
             dst.at<uchar>(y, x) = (grayValue > threshold_value) ? 255 : 0;
         }
     }
-
     return {gray, dst};
 }
