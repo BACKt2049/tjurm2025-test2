9d05dba5918cc15a399f8cb5959c60c2e3efff24
BACKt2049 <2199480387@qq.com>
2024-11-14 22:48:27 +0800

    Update impl.cc

--- src/rect/impl.cc
+++ src/rect/impl.cc
@@ -1,23 +1,26 @@
-#include "impls.h"
-#include <algorithm>
-std::pair<cv::Rect, cv::RotatedRect> get_rect_by_contours(const cv::Mat& input) 
-{
-    std::vector<std::vector<cv::Point>> contours;std::vector<cv::Vec4i> hierarchy;
-    cv::Mat gray;cv::cvtColor(input, gray, cv::COLOR_BGR2GRAY);
-    cv::threshold(gray, gray, 100, 255, cv::THRESH_BINARY_INV);
-    cv::findContours(gray, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+std::pair<cv::Rect, cv::RotatedRect> get_rect_by_contours(const cv::Mat& input) {
     std::pair<cv::Rect, cv::RotatedRect> res;
-    double maxArea = 0;
-    for (size_t i = 0; i < contours.size(); i++)
-        {
-        cv::Rect rect = cv::boundingRect(contours[i]);
-        cv::RotatedRect rotatedRect = cv::minAreaRect(contours[i]);
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::Mat gray;
+    cv::cvtColor(input, gray, cv::COLOR_BGR2GRAY);
+    cv::Mat canny_output;
+    cv::Canny(gray, canny_output, 100, 200);
+    cv::findContours(canny_output, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    double max_area = 0;
+    for (size_t i = 0; i < contours.size(); i++) 
+    {
         double area = cv::contourArea(contours[i]);
-        if (area > maxArea) 
+        if (area > max_area)
         {
-            maxArea = area;
-            res.first = rect;
-            res.second = rotatedRect;
+            max_area = area;
+            cv::Rect bounding_rect = cv::boundingRect(contours[i]);
+            cv::RotatedRect min_area_rect = cv::minAreaRect(contours[i]);
+            res.first = bounding_rect;
+            res.second = min_area_rect;
         }
     }
 
