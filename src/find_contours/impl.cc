d62c17c2d51d7ece4bf8246ce000db2ccdb9ed90
BACKt2049 <2199480387@qq.com>
2024-11-14 22:17:13 +0800

    Update impl.cc

--- src/find_contours/impl.cc
+++ src/find_contours/impl.cc
@@ -1,12 +1,32 @@
-#include "impls.h"
+#include <opencv2/opencv.hpp>
+#include <vector>
 
-std::vector<std::vector<cv::Point>> find_contours(const cv::Mat& input) {
-    std::vector<std::vector<cv::Point>> res;
+std::vector<std::vector<cv::Point>> find_contours(const cv::Mat& input)
+{
+    cv::imshow("Input Image", input);
+    cv::waitKey(0); 
+    std::vector<std::vector<cv::Point>> contours;
     std::vector<cv::Vec4i> hierarchy;
-    cv::Mat gray_image;
-    cv::cvtColor(input, gray_image, cv::COLOR_BGR2GRAY);
-    cv::Mat binary_image;
-    cv::threshold(gray_image, binary_image, 100, 255, cv::THRESH_BINARY);
-    cv::findContours(binary_image, res, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    cv::Mat gray;
+    if (input.channels() == 3) 
+    {
+        cv::cvtColor(input, gray, cv::COLOR_BGR2GRAY);
+    } else
+    {
+        gray = input.clone();
+    }
+    cv::Mat blurred;
+    cv::GaussianBlur(gray, blurred, cv::Size(5, 5), 0);
+    cv::Mat canny_output;
+    cv::Canny(blurred, canny_output, 50, 150);
+    cv::findContours(canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+    std::vector<std::vector<cv::Point>> res; 
+    for (size_t i = 0; i < hierarchy.size(); i++)
+        {
+        if (hierarchy[i][3] == -1 && hierarchy[i][0] == -1)
+        {
+            res.push_back(contours[i]);
+        }
+    }
     return res;
 }
