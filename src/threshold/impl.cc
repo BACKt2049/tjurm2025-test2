a9305ccb8af487b17d4f840ba5b326aeef64ff6f
BACKt2049 <2199480387@qq.com>
2024-11-15 14:01:43 +0800

    Update impl.cc

--- src/threshold/impl.cc
+++ src/threshold/impl.cc
@@ -1,23 +1,8 @@
-#include <vector>
-#include <algorithm>
 #include "impls.h"
-std::vector<cv::Mat> threshold(const cv::Mat& src, int threshold_value) 
+std::vector<cv::Mat> threshold(const cv::Mat& src, int threshold_value)
 {
-    cv::Mat gray(src.size(), CV_8UC1); 
-    cv::Mat dst(src.size(), CV_8UC1);  
-    for (int y = 0; y < src.rows; ++y)
-        {for (int x = 0; x < src.cols; ++x)
-            {cv::Vec3b color = src.at<cv::Vec3b>(y, x);
-            uchar grayValue = static_cast<uchar>(0.299 * color[2] + 0.587 * color[1] + 0.114 * color[0]);
-            gray.at<uchar>(y, x) = grayValue; }
-        }
-    for (int y = 0; y < src.rows; ++y)
-        {
-        for (int x = 0; x < src.cols; ++x) 
-        {
-            uchar grayValue = gray.at<uchar>(y, x);
-            dst.at<uchar>(y, x) = (grayValue > threshold_value) ? 255 : 0;
-        }
-    }
+    cv::Mat gray, dst;
+    cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+    cv::threshold(gray, dst, threshold_value, 255, cv::THRESH_BINARY);
     return {gray, dst};
 }
